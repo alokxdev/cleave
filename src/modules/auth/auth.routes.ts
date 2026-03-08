acc0f7ba8bade256ac8062bc4a2ecfc86b2543f1
Alok <alokforreal@gmail.com>
2026-03-08 16:55:59 +0530

    implement auth routes

---
+++ src/modules/auth/auth.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { validate } from "../../middleware/validate.middleware.js";
+import {
+  registerSchema,
+  loginSchema,
+  refreshTokenSchema,
+} from "./auth.schema.js";
+import { register, login, refreshToken, logout } from "./auth.controller.js";
+
+const router = Router();
+
+router.post("/register", validate({ body: registerSchema }), register);
+router.post("/login", validate({ body: loginSchema }), login);
+router.post("refresh", validate({ body: refreshTokenSchema }), refreshToken);
+router.post("logout", validate({ body: refreshTokenSchema }), logout);
+
+export default router;
