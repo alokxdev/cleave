cd0ce8ab346ed4ac855820d27999d100417b5b94
Alok <alokforreal@gmail.com>
2026-03-10 23:25:38 +0530

    remove createUser and findUserByEmail function

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,20 +1,4 @@
 import { prisma } from "../../db/prisma.js";
-import type { User } from "@prisma/client";
-import { refreshTokenSchema } from "./auth.schema.js";
-
-export const createUser = async (data: {
-  username: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  passwordHash: string;
-}): Promise<User> => prisma.user.create({ data });
-
-export const findUserByEmail = async (email: string): Promise<User | null> =>
-  prisma.user.findUnique({ where: { email } });
-
-export const findUserById = async (id: string): Promise<User | null> =>
-  prisma.user.findUnique({ where: { id } });
 
 export const saveRefreshToken = async (
   userId: string,
